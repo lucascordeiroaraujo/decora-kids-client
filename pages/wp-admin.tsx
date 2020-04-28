d0a70fe31f24351450f89398adca0384ca64cb79
Lucas Cordeiro <lucascordeiroaraujo@gmail.com>
2020-04-28 18:58:08 -0300

    add page redirect to admin

---
+++ pages/wp-admin.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const wpAdminPage = () => (
+  <>
+    <span>redirecionando...</span>
+  </>
+);
+
+interface Iprops {
+  dispatch: any;
+  ctx: any;
+  getInitialProps: any;
+}
+
+wpAdminPage.getInitialProps = async (props: Iprops) => {
+  if (props.ctx.res) {
+    props.ctx.res.writeHead(302, {
+      Location: 'http://decorakidsloja-com-br.umbler.net/wp-login.php'
+    });
+
+    props.ctx.res.end();
+  } else {
+    window.location.href =
+      'http://decorakidsloja-com-br.umbler.net/wp-login.php';
+  }
+};
+
+export default wpAdminPage;
