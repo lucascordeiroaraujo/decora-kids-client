eb274495ef8aa0567941716ca6be0be99e16b80a
Lucas Cordeiro <lucas.cordeiro@octadesk.com>
2020-05-20 10:42:19 -0300

    fix blog post fonts size

--- components/blog/post/style.ts
+++ components/blog/post/style.ts
@@ -19,21 +19,21 @@ export default styled.article`
     font-weight: normal;
   }
   h1 {
-    font-size: calc(1.5rem + ((1vw - 2.5px) * 1.4371));
-    line-height: calc(1.75rem + ((1vw - 4.8px) * 1.8056));
+    font-size: calc(1.5rem + ((1vw - 2.5px) * 0.9581));
+    line-height: calc(1.5625rem + ((1vw - 2.5px) * 1.497));
     margin-bottom: 40px;
     @media (min-width: 1920px) {
-      font-size: 48px;
-      line-height: 54px;
+      font-size: 40px;
+      line-height: 50px;
     }
   }
   h2 {
-    font-size: calc(1.125rem + ((1vw - 2.5px) * 0.7186));
-    line-height: calc(1.375rem + ((1vw - 2.5px) * 0.7186));
+    font-size: calc(1.125rem + ((1vw - 2.5px) * 0.4192));
+    line-height: calc(1.375rem + ((1vw - 2.5px) * 0.479));
     margin-bottom: 80px;
     @media (min-width: 1920px) {
-      font-size: 30px;
-      line-height: 34px;
+      font-size: 25px;
+      line-height: 30px;
     }
   }
   div.author-and-share {
@@ -43,9 +43,9 @@ export default styled.article`
     margin-bottom: 80px;
     span {
       color: ${props => props.theme.colors.secondary};
-      font-size: calc(1.125rem + ((1vw - 2.5px) * 0.3593));
+      font-size: calc(1rem + ((1vw - 2.5px) * 0.2395));
       @media (min-width: 1920px) {
-        font-size: 24px;
+        font-size: 20px;
       }
     }
     & > div {
@@ -78,12 +78,12 @@ export default styled.article`
     }
   }
   p {
-    font-size: calc(1.125rem + ((1vw - 2.5px) * 0.3593));
-    line-height: calc(1.875rem + ((1vw - 2.5px) * 0.3593));
+    font-size: calc(1rem + ((1vw - 2.5px) * 0.2395));
+    line-height: calc(1.5rem + ((1vw - 2.5px) * 0.3593));
     margin-bottom: 50px;
     @media (min-width: 1920px) {
-      font-size: 24px;
-      line-height: 36px;
+      font-size: 20px;
+      line-height: 30px;
     }
   }
   div.both {
