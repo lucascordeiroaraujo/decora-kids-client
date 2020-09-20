46d8a942a48234366dc16941fbe94643014fcac8
Lucas Cordeiro <lucas.cordeiro@octadesk.com>
2020-09-20 19:10:34 -0300

    :wrench: update API URL

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,4 @@
 export const TOKEN_INSTAGRAM =
   '27260130490.2a46ab3.54d5d8ad49ad42d2a14cd2472c5f3bea';
 
-export const URL_API = 'http://decorakidsloja-com-br.umbler.net/?rest_route=';
+export const URL_API = 'http://decorakids-com-br.umbler.net/?rest_route=';
